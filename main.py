fe8066ddeaab6e32be4fc8cc02c89de8dfb7e776
Ismail <imjogiat@gmail.com>
2025-04-15 07:00:00 -0600

    generated pdf pages from csv file using pandas and fpdf

--- main.py
+++ main.py
@@ -6,23 +6,20 @@ import pandas as pd
 pdf_data = pd.read_csv('topics.csv', sep=';')
 # print(f"\n\n{pdf_data}\n\n")
 
-#put the Dataframe object into a dictionary/set that has topic string
-#better way to do it than a nested for loop?
-for index, data in pdf_data.iterrows:
-    pass
-
-
 #Create pdf file object with parameters defined in the implem library file
 refbook_pdf = FPDF(orientation='P', unit= 'mm', format='A4')
 
-refbook_pdf.add_page()
+#put the Dataframe object into a dictionary/set that has topic string
+#better way to do it than a nested for loop?
+for index, row in pdf_data.iterrows():
 
-refbook_pdf.set_font(family='Times', style='B', size=12)
+    refbook_pdf.add_page()
+    refbook_pdf.set_font(family='Times', style='B', size=24)
+    refbook_pdf.set_text_color(100,100,100)
+    refbook_pdf.cell(w=0, h=12, txt=row['Topic'], 
+                align='L', ln=1)
+    refbook_pdf.line(10,25,210,25)
 
-refbook_pdf.cell(w=0, h=12, txt="Hello there!", 
-             align='L', ln=1)
-refbook_pdf.cell(w=0, h=12, txt="Hi there...", 
-             align='L', ln=1)
 
 refbook_pdf.output("refbook_pdf.pdf")
 
